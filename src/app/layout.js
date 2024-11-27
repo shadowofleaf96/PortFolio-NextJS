b3d0fb296a9cf06856a67a3d2234f15d5604cb3e
Shadow Of Leaf <shadowofleaf96@gmail.com>
2024-11-27 16:32:35 +0100

    fix: fix image resize in mobile

--- src/app/layout.js
+++ src/app/layout.js
@@ -30,8 +30,10 @@ export default function RootLayout({ children }) {
                 left: "50%",
                 transform: "translate(-50%, -50%)",
                 zIndex: 1000,
-                width: "10%",
-                height: "20%", 
+                width: "20vw",
+                height: "auto", 
+                maxWidth: "200px",
+                maxHeight: "200px", 
               }}
               alt="Loading..."
             />
